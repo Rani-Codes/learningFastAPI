9bbdda17159f496fa8115266fcb8072242d4e748
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-07-12 20:30:37 -0700

    added hashing to create_user, hashing file incomplete

--- backend/crud.py
+++ backend/crud.py
@@ -1,12 +1,13 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
+from .auth.hashing import get_password_hash
 
 from . import models, schemas
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed" #UPDATE THIS LINE
-    db_user = models.User(username=user.username, hashed_password=fake_hashed_password)
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
