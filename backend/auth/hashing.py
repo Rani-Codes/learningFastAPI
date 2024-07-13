5a16122064f1600267e96c830c42e69f3711c21e
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-07-12 23:05:40 -0700

    Create auth/dependencies file for JWT encoding/decoding and user auth

--- backend/auth/hashing.py
+++ backend/auth/hashing.py
@@ -7,12 +7,3 @@ def get_password_hash(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
