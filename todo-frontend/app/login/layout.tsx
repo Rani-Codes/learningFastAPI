1dfbb0a5485007dc58c9e1f39a10d950601e40fd
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-07-19 23:59:17 -0700

    add login route for next

---
+++ todo-frontend/app/login/layout.tsx
@@ -0,0 +1,21 @@
+// app/login/layout.tsx
+
+import { poppins } from '@/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login - Todo App',
+  description: 'Login to your Todo list web-app',
+};
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased relative overflow-hidden min-h-screen flex items-center justify-center`}>
+        <main className="w-full">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
