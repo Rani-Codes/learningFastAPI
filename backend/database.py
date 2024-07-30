9ebe3cfdd95bd574c60817f92ff0851079948ef7
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-07-29 17:44:09 -0700

    migrate from sqlite db to postgresql db to allow file persistence on backend

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./backend.db"
+# Import dotenv and load environment variables
+from dotenv import load_dotenv
+import os
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
